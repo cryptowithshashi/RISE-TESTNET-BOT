e3f3471a5bee70b49f0f49f9a76ffc39fb5128c8
CRYPTO WITH SHASHI <shashis697723@gmail.com>
2025-04-19 16:34:24 +0530

    Update emitter.js

--- emitter.js
+++ emitter.js
@@ -1,1 +1,12 @@
+// emitter.js
+// Creates and exports a single EventEmitter instance to be shared across modules.
+// This helps prevent circular dependency issues.
+
+const EventEmitter = require('eventemitter3');
+
+// Create a single instance of the event emitter
+const emitter = new EventEmitter();
+
+// Export the instance
+module.exports = emitter;
 
